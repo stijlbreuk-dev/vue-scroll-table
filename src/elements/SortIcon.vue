5a2ac3902837673d5f8605518328e8bddf600c23
Nino Vrijman <ninovrijman@gmail.com>
2018-11-01 17:12:35 +0100

    Apply active class to sort icon based on active sort direction

--- src/elements/SortIcon.vue
+++ src/elements/SortIcon.vue
@@ -1,11 +1,25 @@
 <template>
     <button class="vst_sort-btn">
-        ▲ ▼
+        <span :class="{ s_active: direction === 'ascending'}">
+            ▲
+        </span>
+        <span :class="{ s_active: direction === 'descending'}">
+            ▼
+        </span>
     </button>
 </template>
 
 <script>
 export default {
+    props: ['direction'],
     name: 'sort-icon'
-}
+};
 </script>
+<style lang="scss" scoped>
+    span {
+        opacity: 0.5;
+        &.s_active {
+            opacity: 1;
+        }
+    }
+</style>
