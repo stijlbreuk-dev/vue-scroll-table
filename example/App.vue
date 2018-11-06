835e8e5af73af02f0e5b42650a730f403b8076c5
Nino Vrijman <ninovrijman@gmail.com>
2018-11-06 17:13:06 +0100

    Update example

--- example/App.vue
+++ example/App.vue
@@ -1,223 +1,91 @@
 <template>
-    <div>
+    <div id="app">
+        <h1>Vue Scroll Table</h1>
+        <p>A Vue table component with fully customizable (HTML) cells, horizontal scrolling and a sticky first column.</p>
         <scroll-table :headers="headers"
                       :data="rows"
                       :hasScroll="true"
                       :styles="styles"
                       :classes="classes">
+            <template slot="first name"
+                      slot-scope="{ row, header, index }">
+                <img :src="row[avatarUrlColumnIndex]" />
+                <span>
+                    {{ row[index] }}
+                </span>
+            </template>
+            <template slot="favorite color"
+                      slot-scope="{ row, index }">
+                <svg width="20"
+                     height="20">
+                    <rect width="20"
+                          height="20"
+                          :style="`fill:${row[index]};`"></rect>
+                </svg>
+            </template>
         </scroll-table>
     </div>
 </template>
 <script>
 import ScrollTable from '../src/ScrollTable.vue';
+import data from './assets/data.json';
+
 export default {
-    data() {
-        return {
-            headers: [
-                {
-                    text: 'Name',
-                    sortable: true,
-                    width: 500
-                },
-                {
-                    text: 'Occupation',
-                    sortable: true,
-                    width: 300
-                },
-                {
-                    text: 'Age',
-                    sortable: true,
-                    width: 300
-                },
-                {
-                    text: 'Age',
-                    sortable: true
-                },
-                {
-                    text: 'Age',
-                    sortable: true
-                },
-                {
-                    text: 'Age',
-                    sortable: true
-                },
-                {
-                    text: 'Age',
-                    sortable: true
-                }
-            ],
-            rows: [
-                [
-                    'Aron',
-                    'Front-end Developer',
-                    '37',
-                    'test',
-                    'test',
-                    'test',
-                    'test'
-                ],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27'],
-                ['Aron', 'Front-end Developer', '37'],
-                ['Werner', 'Strategist', '32'],
-                ['Nino', 'Back-end Developer', '22'],
-                ['Jeroen', 'Front-end Developer', '31'],
-                ['Paul', 'Designer', '27']
-            ]
-        };
-    },
-    created() {},
-    computed: {
-        classes() {
-            return {
-                pagination: {
-                    container: [],
-                    limit: {
-                        container: [],
-                        dropdown: []
-                    },
-                    links: {
-                        container: [],
-                        buttons: []
-                    }
-                },
-                tableContainer: [],
-                sticky: {
-                    container: [],
-                    table: [],
-                    tableHeader: [],
-                    tableRow: [],
-                    tableData: []
-                },
-                scroll: {
-                    container: [],
-                    table: [],
-                    tableHeader: [],
-                    tableRow: [],
-                    tableData: []
-                },
-                sortButtons: {
-                    button: [],
-                    active: []
-                }
-            }
-        },
-        styles() {
-            return {
-                pagination: {
-                    container: {
-                    },
-                    limit: {
-                        container: {},
-                        dropdown: {}
-                    },
-                    links: {
-                        container: {},
-                        buttons: {}
-                    }
-                },
-                tableContainer: {
-                },
-                sticky: {
-                    container: {},
-                    table: {},
-                    tableHeader: {},
-                    tableRow: {},
-                    tableData: {}
-                },
-                scroll: {
-                    container: {},
-                    table: {},
-                    tableHeader: {},
-                    tableRow: {},
-                    tableData: {}
-                },
-                sortButtons: {
-                    button: {},
-                    active: {}
-                }
-            };
-        }
-    },
+    name: 'App',
     components: {
         ScrollTable
+    },
+    data() {
+        return {
+            avatarUrlColumnIndex: 10,
+            headers: data.headers,
+            rows: data.rows,
+            styles: {},
+            classes: {
+                tableContainer: ['align-text'],
+                sticky: {
+                    container: ['sticky-column'],
+                    tableHeader: ['header-padding'],
+                    tableData: ['table-cell']
+                },
+                scroll: {
+                    tableHeader: ['header-padding'],
+                    tableData: ['table-cell']
+                }
+            }
+        };
     }
 };
 </script>
+
+<style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .vst_table-container.align-text {
+        text-align: left;
+    }
+
+    .vst_table-container .header-padding {
+        padding: 2rem 1.5rem 1.5rem 1.5rem;
+    }
+
+    .vst_table-container .table-cell {
+        padding: 0.5rem 2rem;
+        vertical-align: middle;
+
+        & img {
+            vertical-align: middle;
+        }
+    }
+
+    .vst_table-container .sticky-column {
+        box-shadow: 10px 0px 10px -12px black;
+    }
+</style>
